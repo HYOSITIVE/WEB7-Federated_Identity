9adeca4e10235303ebb1a79b4d89a49ae4074276
주효식 <lux1937@gmail.com>
2021-04-03 14:50:10 +0000

    Video 7 : Implement update page

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ app.get('/page/:pageId', function(request, response) {
 			var html = template.HTML(sanitizedTitle, list,
 				`<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
 				` <a href="/create">create</a>
-				  <a href="/update?id=${sanitizedTitle}">update</a>
+				  <a href="/update/${sanitizedTitle}">update</a>
 				  <form action="delete_process" method="post">
 					  <input type="hidden" name="id" value="${sanitizedTitle}">
 					  <input type="submit" value="delete">			
@@ -82,8 +82,55 @@ app.post('/create_process', function(request, response) {
 		var title = post.title;
 		var description = post.description;
 		fs.writeFile(`data/${title}`, description, 'utf-8', function(err) {
-			response.writeHead(302, {Location: `/?id=${title}`}); // redirection
+			response.writeHead(302, {Location: `/page/${title}`}); // redirection
 			response.end();
+		});
+	});
+});
+
+app.get('/update/:pageId', function(request, response) {
+	fs.readdir('./data', function(error, filelist) {
+		var	filteredId = path.parse(request.params.pageId).base;			
+		fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+			var title = request.params.pageId;
+			var list = template.list(filelist);
+			var html = template.HTML(title, list,
+				`
+				<form action="/update_process" method="post">
+					<input type="hidden" name="id" value="${title}">
+					<p><input type ="text" name="title" placeholder="title" value="${title}"></p>
+					<p>
+						<textarea name="description" placeholder="description">${description}</textarea>
+					</p>
+					<p>
+						<input type="submit">
+					</p>
+				</form>
+				`,
+				`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+			);
+			response.send(html);
+		});
+	});
+});
+
+
+app.post('/update_process', function(request, response) {
+	var body = '';
+	request.on('data', function(data) {
+		body = body + data;
+	});
+	request.on('end', function() {
+		var post = qs.parse(body);
+		var id = post.id;
+		var title = post.title;
+		var description = post.description;
+		// 기존 파일명(id), 새 파일명(title)을 활용해 파일명 변경. 내용 변경을 위해 callback 함수 호출
+		fs.rename(`data/${id}`, `data/${title}`, function(error) {
+			fs.writeFile(`data/${title}`, description, 'utf-8', function(err) {
+				response.writeHead(302, {Location: `/?id=${title}`}); // redirection
+				response.end();
+			});
 		});
 	});
 });
