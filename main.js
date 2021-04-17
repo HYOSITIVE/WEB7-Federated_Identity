bd1dacceeccb4aefdc2ed52874cd7b331e08cdf0
주효식 <lux1937@gmail.com>
2021-04-17 14:05:03 +0000

    Video 14.2 : Router - Separation as files

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-// Last Modification : 2021.04.15
+// Last Modification : 2021.04.17
 // by HYOSITIVE
-// based on WEB3 - Express - 14.1
+// based on WEB3 - Express - 14.2
 
 var express = require('express')
 var app = express()
 var fs = require('fs');
-var path = require('path');
 var qs = require('querystring');
 var bodyParser = require('body-parser');
-var sanitizeHtml = require('sanitize-html');
-var compression = require('compression')
+var compression = require('compression');
 var template = require('./lib/template.js');
+var topicRouter = require('./routes/topic');
 const port = 3000
 
 app.use(express.static('public')); // public directory 안에서 static file을 찾겠다는 의미. public 폴더 안의 파일은 url을 통해 접근 가능
@@ -49,106 +48,8 @@ app.get('/', function(request, response) { // 결국 Express의 모든 것이 mi
 	response.send(html);
 });
 
-app.get('/topic/create', function(request, response) {
-	var title = 'WEB - create';
-	var list = template.list(request.list);
-	var html = template.HTML(title, list, `
-		<form action="/topic/create_process" method="post">
-			<p><input type ="text" name="title" placeholder="title"></p>
-			<p>
-				<textarea name="description" placeholder="description"></textarea>
-			</p>
-			<p>
-				<input type="submit">
-			</p>
-		</form>
-	`, ''); // control이 존재하지 않기 때문에 argument에 공백 문자 입력
-	response.send(html);
-});
-
-app.post('/topic/create_process', function(request, response) {
-	var post = request.body; // bodyParser가 내부적으로 작동. callback 함수의 request의 body property에 parsing한 내용을 저장
-	console.log(post);
-	var title = post.title;
-	var description = post.description;
-	fs.writeFile(`data/${title}`, description, 'utf-8', function(err) {
-		response.redirect(`/topic/${title}`);
-	});
-});
-
-app.get('/topic/update/:pageId', function(request, response) {
-	var	filteredId = path.parse(request.params.pageId).base;			
-	fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
-		var title = request.params.pageId;
-		var list = template.list(request.list);
-		var html = template.HTML(title, list,
-			`
-			<form action="/topic/update_process" method="post">
-				<input type="hidden" name="id" value="${title}">
-				<p><input type ="text" name="title" placeholder="title" value="${title}"></p>
-				<p>
-					<textarea name="description" placeholder="description">${description}</textarea>
-				</p>
-				<p>
-					<input type="submit">
-				</p>
-			</form>
-			`,
-			`<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-		);
-		response.send(html);
-	});
-});
-
-app.post('/topic/update_process', function(request, response) {
-	var post = request.body;
-	var id = post.id;
-	var title = post.title;
-	var description = post.description;
-	// 기존 파일명(id), 새 파일명(title)을 활용해 파일명 변경. 내용 변경을 위해 callback 함수 호출
-	fs.rename(`data/${id}`, `data/${title}`, function(error) {
-		fs.writeFile(`data/${title}`, description, 'utf-8', function(err) {
-			response.redirect(`/topic/${title}`)
-		});
-	});
-});
-
-app.post('/topic/delete_process', function(request, response) {
-	var post = request.body;
-	var id = post.id;
-	var	filteredId = path.parse(id).base;
-	fs.unlink(`data/${filteredId}`, function(error) {
-		response.redirect('/');
-	});
-});
-
-// 'topic'은 예약어로 사용되므로, /topic으로 접속했을 때에는 topic이라는 제목의 페이지를 탐색하지 않음
-app.get('/topic/:pageId', function(request, response, next) {
-	var	filteredId = path.parse(request.params.pageId).base;
-	fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
-		if (err) {
-			next(err);
-		}
-		else {
-			var title = request.params.pageId;
-			var sanitizedTitle = sanitizeHtml(title);
-			var sanitizedDescription = sanitizeHtml(description, {
-				allowedTags:['h1']
-			});
-			var list = template.list(request.list);
-			var html = template.HTML(sanitizedTitle, list,
-				`<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-				` <a href="/topic/create">create</a>
-			  	<a href="/topic/update/${sanitizedTitle}">update</a>
-			  	<form action="/topic/delete_process" method="post">
-				  <input type="hidden" name="id" value="${sanitizedTitle}">
-				  <input type="submit" value="delete">			
-				</form>`
-			);
-			response.send(html);	
-		}
-	});
-});
+app.use('/topic', topicRouter); // /topic으로 시작하는 주소들에게 topicRouter라는 middleware를 적용
+// 이렇게 사용할 경우, toppicRouter middleware에서 'topic' 경로를 다시 알려줄 필요 없음
 
 app.use(function(req, res, next) { // 404 에러 처리 middleware
 	res.status(404).send('Sorry cant find that!');	
---
+++ routes/topic.js
@@ -0,0 +1,113 @@
+// Last Modification : 2021.04.17
+// by HYOSITIVE
+// based on WEB3 - Express - 14.2
+
+var express = require('express');
+var router = express.Router(); // Router 메소드 호출 시 router라는 객체 return, main.js에서 express라는 모듈 자체는 app이라는 객체를 return
+var path = require('path');
+var fs = require('fs');
+var sanitizeHtml = require('sanitize-html');
+var template = require('../lib/template.js');
+
+router.get('/create', function(request, response) {
+	var title = 'WEB - create';
+	var list = template.list(request.list);
+	var html = template.HTML(title, list, `
+		<form action="/topic/create_process" method="post">
+			<p><input type ="text" name="title" placeholder="title"></p>
+			<p>
+				<textarea name="description" placeholder="description"></textarea>
+			</p>
+			<p>
+				<input type="submit">
+			</p>
+		</form>
+	`, ''); // control이 존재하지 않기 때문에 argument에 공백 문자 입력
+	response.send(html);
+});
+
+router.post('/create_process', function(request, response) {
+	var post = request.body; // bodyParser가 내부적으로 작동. callback 함수의 request의 body property에 parsing한 내용을 저장
+	console.log(post);
+	var title = post.title;
+	var description = post.description;
+	fs.writeFile(`data/${title}`, description, 'utf-8', function(err) {
+		response.redirect(`/topic/${title}`);
+	});
+});
+
+router.get('/update/:pageId', function(request, response) {
+	var	filteredId = path.parse(request.params.pageId).base;			
+	fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+		var title = request.params.pageId;
+		var list = template.list(request.list);
+		var html = template.HTML(title, list,
+			`
+			<form action="/topic/update_process" method="post">
+				<input type="hidden" name="id" value="${title}">
+				<p><input type ="text" name="title" placeholder="title" value="${title}"></p>
+				<p>
+					<textarea name="description" placeholder="description">${description}</textarea>
+				</p>
+				<p>
+					<input type="submit">
+				</p>
+			</form>
+			`,
+			`<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
+		);
+		response.send(html);
+	});
+});
+
+router.post('/update_process', function(request, response) {
+	var post = request.body;
+	var id = post.id;
+	var title = post.title;
+	var description = post.description;
+	// 기존 파일명(id), 새 파일명(title)을 활용해 파일명 변경. 내용 변경을 위해 callback 함수 호출
+	fs.rename(`data/${id}`, `data/${title}`, function(error) {
+		fs.writeFile(`data/${title}`, description, 'utf-8', function(err) {
+			response.redirect(`/topic/${title}`)
+		});
+	});
+});
+
+router.post('/delete_process', function(request, response) {
+	var post = request.body;
+	var id = post.id;
+	var	filteredId = path.parse(id).base;
+	fs.unlink(`data/${filteredId}`, function(error) {
+		response.redirect('/');
+	});
+});
+
+// 'topic'은 예약어로 사용되므로, /topic으로 접속했을 때에는 topic이라는 제목의 페이지를 탐색하지 않음
+router.get('/:pageId', function(request, response, next) {
+	var	filteredId = path.parse(request.params.pageId).base;
+	fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+		if (err) {
+			next(err);
+		}
+		else {
+			var title = request.params.pageId;
+			var sanitizedTitle = sanitizeHtml(title);
+			var sanitizedDescription = sanitizeHtml(description, {
+				allowedTags:['h1']
+			});
+			var list = template.list(request.list);
+			var html = template.HTML(sanitizedTitle, list,
+				`<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+				` <a href="/topic/create">create</a>
+			  	<a href="/topic/update/${sanitizedTitle}">update</a>
+			  	<form action="/topic/delete_process" method="post">
+				  <input type="hidden" name="id" value="${sanitizedTitle}">
+				  <input type="submit" value="delete">			
+				</form>`
+			);
+			response.send(html);	
+		}
+	});
+});
+
+module.exports = router;
