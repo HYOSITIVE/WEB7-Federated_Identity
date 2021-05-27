44ba6d8f0ca0cd5c0e1da6f0ce2ac2625c940d51
주효식 <lux1937@gmail.com>
2021-05-27 14:38:17 +0000

    Video 5 : Session store

--- nodejs/express-session.js
+++ nodejs/express-session.js
@@ -1,17 +1,19 @@
 // Last Modification : 2021.05.27
 // by HYOSITIVE
-// based on WEB4 - Express - Session & Auth - 4
+// based on WEB4 - Express - Session & Auth - 5
 
 var express = require('express')
 var parseurl = require('parseurl')
 var session = require('express-session')
+var FileStore = require('session-file-store')(session)
 
 var app = express()
 
 app.use(session({
   secret: 'keyboard cat', // 타인에게 유출하면 안됨. 실제 구현 시 변수처리하거나 외부에서 지정
   resave: false, // 세션 데이터가 바뀌기 전까지는 세션 저장소에 값을 저장하지 않는다
-  saveUninitialized: true // 세션이 필요하기 전까지는 세션을 구동시키지 않는다
+  saveUninitialized: true, // 세션이 필요하기 전까지는 세션을 구동시키지 않는다
+	store:new FileStore()
 }))
 
 app.get('/', function (req, res, next) {
@@ -21,7 +23,7 @@ app.get('/', function (req, res, next) {
 	} else {
 		req.session.num = req.session.num + 1;
 	}
-  res.send(`Views : ${req.session.num}`); // session middleware는 내부적으로 session storage(세션 저장소)에 num이라는 변수 저장. 세션은 메모리에 저장되어 휘발성이 있음
+  res.send(`Views : ${req.session.num}`); // session middleware는 내부적으로 session store(세션 저장소)에 num이라는 변수 저장. 세션은 메모리에 저장되어 휘발성이 있음
 })
 
 app.listen(3000, function() {
