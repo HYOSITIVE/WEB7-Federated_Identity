24c242d1166ec60986dd8fa1d9e11a2f1830915d
주효식 <lux1937@gmail.com>
2021-05-27 13:40:19 +0000

    Video 3 : Option

--- nodejs/express-session.js
+++ nodejs/express-session.js
@@ -1,6 +1,6 @@
 // Last Modification : 2021.05.27
 // by HYOSITIVE
-// based on WEB4 - Express - Session & Auth - 2
+// based on WEB4 - Express - Session & Auth - 3
 
 var express = require('express')
 var parseurl = require('parseurl')
@@ -9,31 +9,13 @@ var session = require('express-session')
 var app = express()
 
 app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
+  secret: 'keyboard cat', // 타인에게 유출하면 안됨. 실제 구현 시 변수처리하거나 외부에서 지정
+  resave: false, // 세션 데이터가 바뀌기 전까지는 세션 저장소에 값을 저장하지 않는다
+  saveUninitialized: true // 세션이 필요하기 전까지는 세션을 구동시키지 않는다
 }))
 
-app.use(function (req, res, next) {
-  if (!req.session.views) {
-    req.session.views = {}
-  }
-
-  // get the url pathname
-  var pathname = parseurl(req).pathname
-
-  // count the views
-  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
-
-  next()
-})
-
-app.get('/foo', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
-})
-
-app.get('/bar', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
+app.get('/', function (req, res, next) {
+  res.send('Hello session');
 })
 
 app.listen(3000, function() {
