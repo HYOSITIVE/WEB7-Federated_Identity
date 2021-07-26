cd0a80d528fb740d6cf8549b915824f1ebc586f7
Hyosik Philip Joo <lux1937@gmail.com>
2021-07-26 17:41:59 +0900

    Video 7 : Logout

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 // Last Modification : 2021.07.26
 // by HYOSITIVE
-// based on WEB5 - Passport_REWORK - 4.2
+// based on WEB5 - Passport_REWORK - 7
 
 var express = require('express');
 var router = express.Router(); // Router 메소드 호출 시 router라는 객체 return, main.js에서 express라는 모듈 자체는 app이라는 객체를 return
@@ -24,26 +24,12 @@ router.get('/login', function(request, response) {
 	response.send(html);
 });
 
-/*
-router.post('/login_process', function(request, response) {
-	var post = request.body; // bodyParser가 내부적으로 작동. callback 함수의 request의 body property에 parsing한 내용을 저장
-	var email = post.email;
-	var password = post.pwd;
-	if (email === authData.email && password === authData.password) {
-		request.session.is_logined = true;
-		request.session.nickname = authData.nickname;
-		request.session.save(function() { // session middleware는 기록한 데이터를 session store에 기록(메모리에 저장된 세션 데이터를 저장소에 반영)하는데, 기록 작업 종료 이전에 리다이렉션이 이루어질 경우 에러가 발생. 이를 방지하고자 save 함수로 기록하고, save가 끝난 경우 callback 함수로 리다이렉션을 진행
-			response.redirect(`/`);
-		});
-	}
-	else {
-		response.send('Who?');
-	}
-});
-*/
-
 router.get('/logout', function(request, response) {
-	request.session.destroy(function(err) {
+	request.logout();
+	// request.session.destroy(function(err) { // 안전한 로그아웃을 위해 세션 삭제
+	// 	response.redirect('/');
+	// });
+	request.session.save(function(err) { // Egoing님도 살짝 헷갈리는 부분. 세션의 현재 상태 저장
 		response.redirect('/');
 	});
 });
